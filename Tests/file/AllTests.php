f84f4aac6a56fe97f039a48aaf67ef52ec51c8d5
positronium <positronium@17e82a80-5050-0410-bae8-e795ea07cb8d>
2008-06-17 00:32:27 +0000

    File Classes Redesign.

--- Tests/file/AllTests.php
+++ Tests/file/AllTests.php
@@ -12,6 +12,12 @@ require_once 'Tests/file/list/AllTests.php';
 require_once 'Tests/file/yaml/AllTests.php';
 require_once 'Tests/file/ReaderTest.php';
 require_once 'Tests/file/WriterTest.php';
+
+require_once 'Tests/file/NameFilterTest.php';
+require_once 'Tests/file/RegexFilterTest.php';
+require_once 'Tests/file/RecursiveNameFilterTest.php';
+require_once 'Tests/file/RecursiveRegexFilterTest.php';
+
 class Tests_File_AllTests
 {
 	public static function main()
@@ -30,6 +36,11 @@ class Tests_File_AllTests
 		$suite->addTest( Tests_File_Yaml_AllTests::suite() );
 		$suite->addTestSuite('Tests_File_ReaderTest'); 
 		$suite->addTestSuite('Tests_File_WriterTest'); 
+
+		$suite->addTestSuite('Tests_File_NameFilterTest'); 
+		$suite->addTestSuite('Tests_File_RegexFilterTest'); 
+		$suite->addTestSuite('Tests_File_RecursiveNameFilterTest'); 
+		$suite->addTestSuite('Tests_File_RecursiveRegexFilterTest'); 
 		return $suite;
 	}
 }
