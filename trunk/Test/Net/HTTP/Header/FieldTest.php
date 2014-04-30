20565e567828e8ff0dda7aa7ec96c8bc249a7b16
christian.wuerker <christian.wuerker@17e82a80-5050-0410-bae8-e795ea07cb8d>
2014-04-30 08:41:05 +0000

    Updated HTTP test classes.

--- trunk/Test/Net/HTTP/Header/FieldTest.php
+++ trunk/Test/Net/HTTP/Header/FieldTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *	UnitTest for Request Header.
+ *	UnitTest for Request Header Field.
  *	@package		net.http.request
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
  *	@since			16.02.2008
@@ -9,18 +9,18 @@
 require_once 'PHPUnit/Framework/TestCase.php';
 require_once 'Test/initLoaders.php5';
 /**
- *	UnitTest for Request Sender.
+ *	UnitTest for Request Header Field.
  *	@package		net.http.request
  *	@uses			Net_HTTP_Header
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
  *	@since			16.02.2008
  *	@version		0.6
  */
-class Test_Net_HTTP_HeaderTest extends PHPUnit_Framework_TestCase
+class Test_Net_HTTP_Header_FieldTest extends PHPUnit_Framework_TestCase
 {
 	public function testConstruct()
 	{
-		$header	= new Net_HTTP_Header( "key", "value" );
+		$header	= new Net_HTTP_Header_Field( "key", "value" );
 		$assertion	= true;
 		$creation	= (bool) count( $header->toString() );
 		$this->assertEquals( $assertion, $creation );
@@ -28,12 +28,12 @@ class Test_Net_HTTP_HeaderTest extends PHPUnit_Framework_TestCase
 
 	public function testToString()
 	{
-		$header	= new Net_HTTP_Header( "key", "value" );
+		$header	= new Net_HTTP_Header_Field( "key", "value" );
 		$assertion	= "Key: value";
 		$creation	= $header->toString();
 		$this->assertEquals( $assertion, $creation );
 
-		$header	= new Net_HTTP_Header( "key-with-more-words", "value" );
+		$header	= new Net_HTTP_Header_Field( "key-with-more-words", "value" );
 		$assertion	= "Key-With-More-Words: value";
 		$creation	= $header->toString();
 		$this->assertEquals( $assertion, $creation );
