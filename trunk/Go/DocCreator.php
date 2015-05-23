8d7aa4f1deb20c97ecfc2328d99917b5a868ee08
christian.wuerker <christian.wuerker@17e82a80-5050-0410-bae8-e795ea07cb8d>
2015-05-23 07:21:07 +0000

    Updated Go classes.

--- trunk/Go/DocCreator.php
+++ trunk/Go/DocCreator.php
@@ -4,7 +4,7 @@ class Go_DocCreator
 	public function __construct( $arguments )
 	{
 		$config		= Go_Library::getConfigData();
-		require_once dirname( dirname( __FILE__ ) ).'/autoload.php5';
+		require_once dirname( dirname( __FILE__ ) ).'/autoload.php';
 		$path	= $config['docCreator']['pathTool'];
 		if( !file_exists( $path ) )
 			throw new Exception( 'Tool "DocCreator" is not installed' );
--- trunk/Go/SelfTester.php
+++ trunk/Go/SelfTester.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname( __FILE__ ).'/Library.php5';
+require_once dirname( __FILE__ ).'/Library.php';
 class Go_SelfTester
 {
 	public function __construct( $arguments )
@@ -10,10 +10,10 @@ class Go_SelfTester
 
 		$path	= dirname( __FILE__ );
 		$data	= Go_Library::listClasses( $path );
-		
+
 		Go_Library::testSyntax( $data['files'] );
 		Go_Library::testImports( $data['files'] );
-		
+
 		remark( "create random numbers with 3 digits: " );
 		import( 'de.ceus-media.alg.Randomizer' );
 		import( 'de.ceus-media.math.RomanNumbers' );
