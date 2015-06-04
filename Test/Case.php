1dff515eddf5c656fe249949ee8a7a74a8924403
Christian Würker <kriss@ceusmedia.de>
2015-06-04 10:39:02 +0200

    Update test classes.

--- Test/Case.php
+++ Test/Case.php
@@ -4,5 +4,7 @@ if( !class_exists( 'PHPUnit_Framework_TestCase' ) )
 	require_once 'PHPUnit/Framework/TestCase.php';
 class Test_Case extends PHPUnit_Framework_TestCase{
 	static public $config;
+	static public $pathLib;
 }
+Test_Case::$pathLib	= dirname( __DIR__  ).'/';
 ?>
