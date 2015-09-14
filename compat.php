b56d325ff4b94adfe991c202c3a81a73c282dbfe
Christian Würker <kriss@ceusmedia.de>
2015-09-14 21:52:09 +0200

    Extend compatibility script.

--- compat.php
+++ compat.php
@@ -7,9 +7,12 @@ if( $isComposer && $isFromGithub ){
     class Database_PDO_DataSourceName extends DB_PDO_DataSourceName{}
 	class File_JSON_Reader extends FS_File_JSON_Reader{}
 	class File_Reader extends FS_File_Reader{}
+	class Folder_Lister extends FS_Folder_Lister{}
+	class Folder_RecursiveLister extends FS_Folder_RecursiveLister{}
 	class CMM_Bootstrap_PageControl extends \CeusMedia\Bootstrap\PageControl{}
 	class File_RecursiveRegexFilter extends FS_File_RecursiveRegexFilter{}
 	class CMM_OSQL_Client{}
+	class CMC_Loader extends Loader{}
 	new UI_DevOutput;
 }
 
