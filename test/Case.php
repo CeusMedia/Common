144cee59d766e398f8c920dee5bad452ae588b29
kriss0r <christian.wuerker@check24.de>
2019-11-08 18:19:02 +0100

    Improve testing.

--- test/Case.php
+++ test/Case.php
@@ -1,7 +1,9 @@
 <?php
 //require_once 'PHPUnit/Framework/TestCase.php';
-if( !class_exists( 'PHPUnit_Framework_TestCase' ) )
-	require_once 'PHPUnit/Framework/TestCase.php';
+if( !class_exists( 'PHPUnit_Framework_TestCase' ) ){
+	class PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase{}
+}
+//	require_once 'PHPUnit/Framework/TestCase.php';
 class Test_Case extends PHPUnit_Framework_TestCase{
 	static public $config;
 	static public $pathLib;
