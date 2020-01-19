99fc4d90ce1095304a1c921ab80d8ee727db4307
Christian Würker <kriss@ceusmedia.de>
2020-01-19 06:42:46 +0100

    Migrate test bootstrap (incomplete).

--- test/Case.php
+++ test/Case.php
@@ -1,4 +1,14 @@
 <?php
+declare( strict_types = 1 );
+
+use PHPUnit\Framework\TestCase;
+
+class Test_Case extends TestCase{
+	public static $config;
+}
+
+return;
+
 //require_once 'PHPUnit/Framework/TestCase.php';
 if( !class_exists( 'PHPUnit_Framework_TestCase' ) ){
 	class PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase{}
@@ -8,6 +18,6 @@ class Test_Case extends PHPUnit_Framework_TestCase{
 	static public $config;
 	static public $pathLib;
 }
-Test_Case::$pathLib	= dirname( __DIR__  ).'/';
+//Test_Case::$pathLib	= dirname( __DIR__  ).'/';
 error_reporting( error_reporting() || ~E_USER_DEPRECATED );
 ?>
