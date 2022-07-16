5e7ca4d73f3384c1f1d201f817f38c7a6102a592
kriss0r <kriss@ceusmedia.de>
2022-07-16 04:33:39 +0200

    Fix minor doc bug.

--- src/XML/Element.php
+++ src/XML/Element.php
@@ -174,7 +174,7 @@ class XML_Element extends SimpleXMLElement
 	 *	@access		public
 	 *	@param		string		$name		Name of attribute
 	 *	@param		string		$nsPrefix	Namespace prefix of attribute
-	 *	@return		bool
+	 *	@return		string
 	 *	@throws		RuntimeException		if attribute is not set
 	 */
 	public function getAttribute( $name, $nsPrefix = NULL )
