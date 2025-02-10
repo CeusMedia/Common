6abd4fa3c91040913ff1c92408ab028b310909f8
kriss0r <kriss@ceusmedia.de>
2025-02-10 10:57:51 +0100

    Update copyright year.

--- src/Exception/NotEnabled.php
+++ src/Exception/NotEnabled.php
@@ -4,7 +4,7 @@
  *	Exception for anything no being enabled.
  *	Is serializable (to PHP, JSON), renderable and describable.
  *
- *	Copyright (c) 2024 Christian Würker (ceusmedia.de)
+ *	Copyright (c) 2024-2025 Christian Würker (ceusmedia.de)
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -22,7 +22,7 @@
  *	@category		Library
  *	@package		CeusMedia_Common_Exception
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
- *	@copyright		2024 Christian Würker
+ *	@copyright		2024-2025 Christian Würker
  *	@license		https://www.gnu.org/licenses/gpl-3.0.txt GPL 3
  *	@link			https://github.com/CeusMedia/Common
  */
@@ -41,7 +41,7 @@ use CeusMedia\Common\Exception\Traits\Serializable as SerializableTrait;
  *	@category		Library
  *	@package		CeusMedia_Common_Exception
  *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
- *	@copyright		2024 Christian Würker
+ *	@copyright		2024-2025 Christian Würker
  *	@license		https://www.gnu.org/licenses/gpl-3.0.txt GPL 3
  *	@link			https://github.com/CeusMedia/Common
  */
